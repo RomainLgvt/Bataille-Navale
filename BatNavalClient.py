373d0673c802a385ca031a2d42c7f00d20cd9562
RomainLgvt <ligavant@lacroixrouge-brest.fr>
2018-12-10 16:15:09 +0100

    first commit

---
+++ BatNavalClient.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP ="10.160.108.2"
+UDP_PORT = 5005
+MESSAGE = "Connection au serveur"
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print("Réponse serveur :", data.decode())
+    MESSAGE=input("Votre Reponse :")
+    sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
